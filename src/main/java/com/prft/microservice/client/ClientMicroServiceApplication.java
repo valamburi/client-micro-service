e51e18fff7ab960e2b0c4747e087466a0f937ee3
Valamburi Murugan <valamburi.murugan@perficient.com>
2019-01-04 17:31:29 +0530

    Initial Commit

---
+++ src/main/java/com/prft/microservice/client/ClientMicroServiceApplication.java
@@ -0,0 +1,14 @@
+package com.prft.microservice.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientMicroServiceApplication.class, args);
+	}
+
+}
+
